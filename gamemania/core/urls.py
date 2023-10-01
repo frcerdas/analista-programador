b2d9df85ec83550974097543ff076d89d8b77b57
diosses <di.osses@duocuc.cl>
2023-10-01 19:01:56 -0300

    add login

--- gamemania/core/urls.py
+++ gamemania/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import inicio, categoria_detail, login, registro, addgame
+from .views import inicio, categoria_detail, inicio_sesion, registro, addgame
 
 # app_name = "core"   
 
@@ -7,7 +7,7 @@ from .views import inicio, categoria_detail, login, registro, addgame
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('categoria/<str:slug_categoria>/', categoria_detail, name='categoria_detail'),
-    path('login/', login, name='login'),
+    path('login/', inicio_sesion, name='login'),
     path('registro/', registro, name='registro'),
     path('agregar-juego', addgame, name='addgame'),
 ]
--- gamemania/core/views.py
+++ gamemania/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categoria, Videojuego
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -17,9 +18,22 @@ def categoria_detail(request, slug_categoria):
     videojuegos_por_categoria = Videojuego.objects.filter(categoria=categoria)
     return render(request, 'main/page-category.html', {'categoria': categoria, 'categorias': categorias, 'videojuegos_por_categoria': videojuegos_por_categoria})
 
-def login(request):
+def inicio_sesion(request):
+    if request.method == 'POST':
+        usuario = request.POST.get('campo-nickname')
+        contraseña = request.POST.get('campo-password')
+        user = authenticate(request, username=usuario, password=contraseña)
+        if user is not None:
+            login(request, user)
+            categoria = Categoria.objects.all()
+            return render(request = request, template_name="main/index.html",context={'categoria':categoria})
+        else:
+            context = {
+                'error' : 'Error, intente nuevamente'
+            }
+            return render(request, 'main/auth/login.html', context)
     categoria = Categoria.objects.all()
-    return render(request, 'main/login.html',context={'categoria':categoria})
+    return render(request, 'main/auth/login.html',context={'categoria':categoria})
 
 def registro(request):
     categoria = Categoria.objects.all()
